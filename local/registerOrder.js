2e859ff1832791409db70e95566cf5f7ab15336d
Eitaro Inagaki <ata@netkoubou.com>
2015-10-23 17:55:00 +0900

    changed local/registerOrder.js from slapdash codes to skelton (imcomplete codes).

--- local/registerOrder.js
+++ local/registerOrder.js
@@ -1,6 +1,17 @@
 'use strict';
+var util = require('./util');
 
 module.exports = function(req, res) {
-    console.log(req.body);
-    res.json({ status: 0, order_code: '0893' });
+    var order_type      = req.body.order_type;
+    var order_remark    = req.body.order_remark;
+    var department_code = req.body.department_code;
+    var trader_code     = req.body.trader_code;
+    var products        = req.body.products;
+
+    var order_code;
+    var order = {};
+
+    db.collection('orders').insert(order);
+
+    res.json({ status: 0, order_code: order_code });
 };
