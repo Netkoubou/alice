52382048111e50dfda474ae26b9197cf9925e09b
Eitaro Inagaki <ata@netkoubou.com>
2015-11-10 10:07:43 +0900

    made footer message animation.

--- public/js/Footer.js
+++ public/js/Footer.js
@@ -2,13 +2,15 @@
 var React  = require('react');
 var Footer = React.createClass({
     getInitialState: function() {
-        return ({ message: '開国して下さい!' });
+        return ({ message: '1854 年 3 月 31 日 開国です! (具体的には日米和親条約調印)' });
     },
 
     render: function() {
         return (
             <footer id="footer">
-              {this.state.message}
+              <div id="footer-telop">
+                <p id="footer-text">{this.state.message}</p>
+              </div>
             </footer>
         );
     }
