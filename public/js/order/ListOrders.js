dfe046abf81e23fee4c4a52cf188554783d47710
Eitaro Inagaki <ata@netkoubou.com>
2015-12-02 14:20:53 +0900

    refactored.

--- public/js/order/ListOrders.js
+++ public/js/order/ListOrders.js
@@ -152,95 +152,7 @@ var ListOrders = React.createClass({
                 throw 'server_searchOrders';
             }
 
-            var orders = res.body.orders.map(function(order, index) {
-                var order_state = Util.toOrderStateName(order.order_state);
-                var order_type  = Util.toOrderTypeName(order.order_type);
-
-                if (order.order_type === 'URGENCY_ORDER') {
-                    if (order.order_state != 'COMPLETED') {
-                        if (order.order_state != 'NULLIFIED') {
-                            order_type = <span className="list-orders-urgency">
-                                           {order_type}
-                                         </span>
-                        }
-                    }
-                }
-
-                var order_total   = 0.0;
-                var billing_total = 0;
-
-                order.products.forEach(function(f) {
-                    order_total   += f.cur_price * f.quantity;
-                    billing_total += f.billing_amount;
-                });
-
-                order_total = Math.round(order_total);
-
-                var order_remark = '';
-
-                if (order.order_remark != '') {
-                    var id      = 'list-orders-popover' + index.toString();
-                    var popover = <Popover id={id} title="備考・連絡">
-                                    {order.order_remark}
-                                  </Popover>;
-
-                    order_remark = (
-                        <OverlayTrigger container={this.refs.listOrders}
-                                        placement="left"
-                                        overlay={popover}>
-                          <span className="list-orders-remark">!</span>
-                        </OverlayTrigger>
-                    );
-                }
-
-                return [
-                    {   // 起案番号
-                        value: order.order_code,
-                        view:  <OrderCode user={this.props.user}
-                                          order={order}
-                                          goBack={this.backToHere}
-                                          onSelect={this.onSelect} />
-                    },
-                    {   // 起案日
-                        value: order.drafting_date,
-                        view:  order.drafting_date
-                    },
-                    {   // 起案者
-                        value: order.drafter_account,
-                        view:  order.drafter_account
-                    },
-                    {   // 発注区分
-                        value: order_type,
-                        view:  order_type
-                    },
-                    {   // 発注元 部門診療科
-                        value: order.department_name,
-                        view:  order.department_name
-                    },
-                    {   // 発注先 販売元
-                        value: order.trader_name,
-                        view:  order.trader_name
-                    },
-                    {   // 発注総計
-                        value: order_total,
-                        view:  order_total.toLocaleString()
-                    },
-                    {   // 請求総計
-                        value: billing_total,
-                        view:  billing_total.toLocaleString()
-                    },
-                    {   // 状態
-                        value: order_state,
-                        view:  order_state
-                    },
-                    {   // 備考・連絡
-                        value: '',
-                        view:  order_remark,
-                    }
-                ];
-            }.bind(this) );
-
-            this.setState({ orders: orders });
+            this.setState({ orders: res.body.orders });
         }.bind(this) )
     },
 
@@ -254,13 +166,8 @@ var ListOrders = React.createClass({
         });
     },
 
-    render: function() {
-        if (this.state.next_ope != null) {
-            return this.state.next_ope;
-        }
-        
-        // LIST_ORDERS
-        var title = [
+    makeTableFrameTitle: function() {
+        return [
             { name: '起案番号',          type: 'string' },
             { name: '起案日',            type: 'string' },
             { name: '起案者',            type: 'string' },
@@ -272,60 +179,179 @@ var ListOrders = React.createClass({
             { name: '状態',              type: 'string' },
             { name: '!',                 type: 'void' }
         ];
+    },
+
+    decideOrderTypeView: function(order) {
+        var view  = Util.toOrderTypeName(order.order_type);
+
+        if (order.order_type === 'URGENCY_ORDER') {
+            if (order.order_state != 'COMPLETED') {
+                if (order.order_state != 'NULLIFIED') {
+                    return (
+                        <span className="list-orders-urgency">
+                          {view}
+                        </span>
+                    );
+                }
+            }
+        }
+
+        return view;
+    },
+
+    decideOrderRemark: function(order, index) {
+        if (order.order_remark != '') {
+            var id      = 'list-orders-popover' + index.toString();
+            var popover = <Popover id={id} title="備考・連絡">
+                            {order.order_remark}
+                          </Popover>;
+
+            return (
+                <OverlayTrigger container={this.refs.listOrders}
+                                placement="left"
+                                overlay={popover}>
+                  <span className="list-orders-remark">!</span>
+                </OverlayTrigger>
+            );
+        }
+
+        return '';
+    },
+
+    composeTableFrameData: function() {
+        return this.state.orders.map(function(order, index) {
+            var order_state      = Util.toOrderStateName(order.order_state);
+            var order_type_view  = this.decideOrderTypeView(order);
+
+            var order_total   = 0.0;
+            var billing_total = 0;
+
+            order.products.forEach(function(f) {
+                order_total   += f.cur_price * f.quantity;
+                billing_total += f.billing_amount;
+            });
+
+            order_total = Math.round(order_total);
+
+            return [
+                {   // 起案番号
+                    value: order.order_code,
+                    view:  <OrderCode user={this.props.user}
+                                      order={order}
+                                      goBack={this.backToHere}
+                                      onSelect={this.onSelect} />
+                },
+                {   // 起案日
+                    value: order.drafting_date,
+                    view:  order.drafting_date
+                },
+                {   // 起案者
+                    value: order.drafter_account,
+                    view:  order.drafter_account
+                },
+                {   // 発注区分
+                    value: order.order_type,
+                    view:  order_type_view
+                },
+                {   // 発注元 部門診療科
+                    value: order.department_name,
+                    view:  order.department_name
+                },
+                {   // 発注先 販売元
+                    value: order.trader_name,
+                    view:  order.trader_name
+                },
+                {   // 発注総計
+                    value: order_total,
+                    view:  order_total.toLocaleString()
+                },
+                {   // 請求総計
+                    value: billing_total,
+                    view:  billing_total.toLocaleString()
+                },
+                {   // 状態
+                    value: order_state,
+                    view:  order_state
+                },
+                {   // 備考・連絡
+                    value: '',
+                    view:  this.decideOrderRemark(order, index)
+                }
+            ];
+        }.bind(this) );
+    },
+
+
+    /*
+     * ref があるため、component とはしない (というかできない)
+     */
+    generateSearchPain: function() {
+        return (
+            <fieldset id="list-orders-search">
+              <legend>検索</legend>
+              <div id="list-orders-calendar-marker">
+                <CalendarMarker startDate={this.state.start_date}
+                                endDate={this.state.end_date}
+                                onMark={this.onMark} />
+              </div>
+              <div className="list-orders-checkbox">
+                <Input type="checkbox"
+                       label="依頼中"
+                       checked={this.state.is_requesting}
+                       onChange={this.onChangeCheckbox}
+                       ref="requesting" />
+              </div>
+              <div className="list-orders-checkbox">
+                <Input type="checkbox"
+                       label="承認待ち"
+                       checked={this.state.is_approving}
+                       onChange={this.onChangeCheckbox}
+                       ref="approving" />
+              </div>
+              <div className="list-orders-checkbox">
+                <Input type="checkbox"
+                       label="承認済み"
+                       checked={this.state.is_approved}
+                       onChange={this.onChangeCheckbox}
+                       ref="approved" />
+              </div>
+              <div className="list-orders-checkbox">
+                <Input type="checkbox"
+                       label="無効"
+                       checked={this.state.is_nullified}
+                       onChange={this.onChangeCheckbox}
+                       ref="nullified" />
+              </div>
+              <div className="list-orders-checkbox">
+                <Input type="checkbox"
+                       label="完了"
+                       checked={this.state.is_completed}
+                       onChange={this.onChangeCheckbox}
+                       ref="completed" />
+              </div>
+              <div id="list-orders-buttons">
+                <Button bsSize="small" onClick={this.onSearch}>検索</Button>
+              </div>
+            </fieldset>
+        );
+    },
+
+    render: function() {
+        if (this.state.next_ope != null) {
+            return this.state.next_ope;
+        }
+        
+        var title = this.makeTableFrameTitle();
+        var data  = this.composeTableFrameData();
 
         return (
             <div id="list-orders" ref="listOrders">
-              <fieldset id="list-orders-search">
-                <legend>検索</legend>
-                <div id="list-orders-calendar-marker">
-                  <CalendarMarker startDate={this.state.start_date}
-                                  endDate={this.state.end_date}
-                                  onMark={this.onMark} />
-                </div>
-                <div className="list-orders-checkbox">
-                  <Input type="checkbox"
-                         label="依頼中"
-                         checked={this.state.is_requesting}
-                         onChange={this.onChangeCheckbox}
-                         ref="requesting" />
-                </div>
-                <div className="list-orders-checkbox">
-                  <Input type="checkbox"
-                         label="承認待ち"
-                         checked={this.state.is_approving}
-                         onChange={this.onChangeCheckbox}
-                         ref="approving" />
-                </div>
-                <div className="list-orders-checkbox">
-                  <Input type="checkbox"
-                         label="承認済み"
-                         checked={this.state.is_approved}
-                         onChange={this.onChangeCheckbox}
-                         ref="approved" />
-                </div>
-                <div className="list-orders-checkbox">
-                  <Input type="checkbox"
-                         label="無効"
-                         checked={this.state.is_nullified}
-                         onChange={this.onChangeCheckbox}
-                         ref="nullified" />
-                </div>
-                <div className="list-orders-checkbox">
-                  <Input type="checkbox"
-                         label="完了"
-                         checked={this.state.is_completed}
-                         onChange={this.onChangeCheckbox}
-                         ref="completed" />
-                </div>
-                <div id="list-orders-buttons">
-                  <Button bsSize="small" onClick={this.onSearch}>検索</Button>
-                </div>
-              </fieldset>
+              {this.generateSearchPain()}
               <fieldset id="list-orders-table-frame">
                 <legend>発注一覧</legend>
                 <TableFrame id="list-orders-orders"
                             title={title}
-                            data={this.state.orders} />
+                            data={data} />
               </fieldset>
             </div>
         );
