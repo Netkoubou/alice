0d1184f1ce1f07bf9d9c3ebc74834bd54359193a
Eitaro Inagaki <ata@netkoubou.com>
2015-12-03 16:13:15 +0900

    refactored.

--- local/js/searchCandidates.js
+++ local/js/searchCandidates.js
@@ -7,6 +7,7 @@ var log_info = log4js.getLogger('info');
 var log_warn = log4js.getLogger('warning');
 var log_crit = log4js.getLogger('critical');
 
+
 module.exports = function(req, res) {
     if (req.session.user == null) {
         res.json({ status: 255 });
@@ -14,9 +15,55 @@ module.exports = function(req, res) {
         return;
     }
 
+    var is_already_sent = false;
+    var candidates      = [];
+
+    function lookup_trader_name(db, product, num_of_candidates) {
+        var id     = new ObjectID(product.trader_code);
+        var cursor = db.collection('traders').find({ _id: id});
+
+        cursor.limit(1).next(function(err, trader) {
+            if (is_already_sent) {
+                return;
+            }
+
+            if (err == null && trader != null) {
+                candidates.push({
+                    product_code: product._id,
+                    product_name: product.name,
+                    maker:        product.maker,
+                    min_price:    product.min_price,
+                    cur_price:    product.cur_price,
+                    max_price:    product.max_price,
+                    trader_code:  trader._id,
+                    trader_name:  trader.name
+                });
+
+                if (candidates.length == num_of_candidates) {
+                    res.json({ status: 0, candidates: candidates });
+                    is_already_sent = true;
+                    db.close();
+                }
+            } else {
+                db.close();
+                res.json({ status: 255 });
+                is_already_sent = true;
+
+                if (err != null) {
+                    log_warn.warn(err);
+                }
+
+                var msg = '[searchCandidates] failed to find trader: "' +
+                          product.trader_code + '".';
+
+               log_warn.warn(msg);
+            }
+        });
+    }
+
     util.query(function(db) {
         var sel = {
-            is_alive:    true,
+            is_alive:         true,
             department_codes: new ObjectID(req.body.department_code)
         };
 
@@ -33,65 +80,16 @@ module.exports = function(req, res) {
         }
 
         db.collection('products').find(sel).toArray(function(err, products) {
-            var msg;
-
             if (err == null && products != null) {
                 if (products.length == 0) {
                     res.json({ status: 0, candidates: [] });
                 } else {
-                    var is_already_sent = false;
-                    var candidates      = [];
-
                     products.forEach(function(p) {
                         if (is_already_sent) {
                             return;
                         }
 
-                        var id     = new ObjectID(p.trader_code);
-                        var cursor = db.collection('traders').find({ _id: id});
-
-                        cursor.limit(1).next(function(err, trader) {
-                            if (is_already_sent) {
-                                return;
-                            }
-
-                            if (err == null && trader != null) {
-                                candidates.push({
-                                    product_code: p._id,
-                                    product_name: p.name,
-                                    maker:        p.maker,
-                                    min_price:    p.min_price,
-                                    cur_price:    p.cur_price,
-                                    max_price:    p.max_price,
-                                    trader_code:  trader._id,
-                                    trader_name:  trader.name
-                                });
-
-                                if (candidates.length == products.length) {
-                                    res.json({
-                                        status: 0,
-                                        candidates: candidates
-                                    });
-
-                                    is_already_sent = true;
-                                    db.close();
-                                }
-                            } else {
-                                db.close();
-                                res.json({ status: 255 });
-                                is_already_sent = true;
-
-                                if (err != null) {
-                                    log_warn.warn(err);
-                                }
-
-                                msg = '[searchCandidates] ' +
-                                      'failed to find trader: "' +
-                                      p.trader + '".';
-
-                               log_warn.warn(msg);
-                            }
-                        });
+                        lookup_trader_name(db, p, products.length);
                     });
                 }
             } else {
@@ -102,8 +100,8 @@ module.exports = function(req, res) {
                     log_warn.warn(err);
                 }
 
-                msg = '[searchCandidates] ' +
-                      'failed to find in "products" collection.';
+                var msg = '[searchCandidates] ' +
+                          'failed to find in "products" collection.';
 
                 log_warn.warn(msg);
             }
