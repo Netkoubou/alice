316d1aff20ae48c3ae66f7e1bb76b7db01915db5
Eitaro Inagaki <ata@netkoubou.com>
2016-01-26 17:13:53 +0900

    prepared to draw graph for budget and outgo.

--- public/js/budget/DrawIncomeAndOutgoGraph.jsx
+++ public/js/budget/DrawIncomeAndOutgoGraph.jsx
@@ -1,17 +1,23 @@
 'use strict';
 var React    = require('react');
 var ReactDOM = require('react-dom');
+var d3       = require('d3');
 
 var DrawIncomeAndOutgoGraph = React.createClass({
+    componentDidMount: function() {
+        d3.select(this.refs.graph)
+           .append("p")
+           .text("New paragraph!");
+    },
+
     render: function() {
         return (
-            <div>
-            </div>
+            <div ref="graph"></div>
         );
     }
 });
 
 ReactDOM.render(
-    <DrawIncomeAndOutgoGraph />, 
+    <DrawIncomeAndOutgoGraph width={768} height={1280} />, 
     document.getElementById('contents-area')
 );
